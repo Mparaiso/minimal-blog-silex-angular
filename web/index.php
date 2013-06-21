d57d23db23cd70c5abdc3ba9c2b77b38d9555a64
mparaiso <aikah@free.fr>
2013-06-21 09:51:26 +0200

    index.php

--- web/index.php
+++ web/index.php
@@ -1,12 +1,16 @@
 <?php
 
+
+
 $filename = __DIR__ . preg_replace('#(\?.*)$#', '', $_SERVER['REQUEST_URI']);
 if (php_sapi_name() === 'cli-server' && is_file($filename)) {
     return FALSE;
 }
 
 $autoload = require __DIR__ . "/../vendor/autoload.php";
-$debut = getenv("NETTUTS_LARAVEL_BACKBONE_ENV") == "development" ? TRUE : FALSE;
+
+
+$debug = getenv("NETTUTS_LARAVEL_BACKBONE_ENV") == "development" ? TRUE : FALSE;
 $app = new App(array("debug" => $debug));
 
 
